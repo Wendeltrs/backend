7a0ecc842cb174b39f16b967c8c9c7836c8ac7df
Wendel Tavares <wendeljt17@gmail.com>
2024-08-19 19:40:50 -0300

    fastify's plugins

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import { randomUUID } from 'crypto'
-import { title } from 'process'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-/*
-const transaction = await knex('transactions').insert({  INSERINDO DADOS NA TABELA
-    id: randomUUID(),
-    amount: 1000
-})
-return transaction
-*/
-app.get('/hello', async () => { //Método GET
-    const transaction = await knex('transactions')
-        .where('amount', 1000)
-        .select('*')
-
-    return transaction
-})
+app.register(transactionsRoutes) //Definindo plug-ins
 
 app.listen({
     port: env.PORT //Porta
