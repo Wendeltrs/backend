e8c3e498afa60ab9dc0d2923191844b74ceec0e0
Wendel Tavares <wendeljt17@gmail.com>
2024-08-20 10:08:08 -0300

    update test

--- src/test/transactions.test.ts
+++ src/test/transactions.test.ts
@@ -1,6 +1,6 @@
 import { afterAll, beforeAll, test } from 'vitest'
 import request from 'supertest'
-import { app } from './../app'
+import { app } from '../app'
 
 beforeAll(async () => { //Antes de executar os testes, aguarda todas as transações do servidor serem executadas
     await app.ready()
